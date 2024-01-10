a09d90fe081e187cc1ed167ca4025822066e2b3e
SANKET PATADE <118457157+patadesanket@users.noreply.github.com>
2024-01-10 19:41:57 +0530

    Add files via upload
    This is the basic calculator  which consist  simple operations like addition ,subtraction ,multiply and divide ,

---
+++ calculator.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     10-01-2024
+# Copyright:   (c) User 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+x=int(input('enter a number'));
+y=int(input('enter another num'));
+
+sum=x+y;
+print("Addition of two number is ",sum);
+
+sub=x-y;
+print("subtraction of two numbers is",sub);
+
+mul=x*y;
+print("multiplication of two numbers is",mul);
+
+div=x/y;
+print("division of tweo numbers is ", div);
